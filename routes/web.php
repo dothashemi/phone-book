60c4bb506c29f9ddebecb7152690579edc8ef791
Mohamed Hashemi <mh.jezireti@gmail.com>
2020-01-06 06:52:53 +0330

    edited route and controller for Home page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
@@ -26,5 +24,4 @@ Route::prefix('panel')->middleware('auth')->group(function () {
     Route::resource('/phones', 'ContactController');
 
     Route::resource('/groups', 'ContactController');
-
 });

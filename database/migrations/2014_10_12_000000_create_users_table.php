5b30f34d68dbcab300788d51b3f06cc3ed252021
Mohamed Hashemi <mh.jezireti@gmail.com>
2020-01-09 12:03:00 +0330

    edited UserController

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name', 100);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
